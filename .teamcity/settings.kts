2f50d94972c3d4530b1a55f230601e279f9e0565
Brian Joseph Donohoe <brian.donohoe@statnett.no>
2025-09-30 11:33:16 +0200

    Fix sonar scan

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,19 +1,85 @@
-import no.elhub.devxp.build.configuration.pipeline.ElhubProject.Companion.elhubProject
+import jetbrains.buildServer.configs.kotlin.ArtifactRule
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import no.elhub.devxp.build.configuration.pipeline.constants.AgentScope
 import no.elhub.devxp.build.configuration.pipeline.constants.Group.DEVXP
+import no.elhub.devxp.build.configuration.pipeline.dsl.elhubProject
+import no.elhub.devxp.build.configuration.pipeline.jobs.customJob
 import no.elhub.devxp.build.configuration.pipeline.jobs.makeVerify
 
 elhubProject(DEVXP, "devxp-lint-configuration") {
     pipeline {
         sequential {
-            makeVerify {
-                sonarScanSettings = {
-                    sonarProjectSources = "resources/,Makefile"
+            val prepareFilesForSonarJob = customJob(
+                AgentScope.LinuxAgentContext,
+                buildArtifactRules = listOf(ArtifactRule.include("resources-sonar-temp", "resources-sonar-temp.zip")),
+                outputArtifactRules = listOf(ArtifactRule.include("resources-sonar-temp.zip", "resources-sonar-temp"))
+            ) {
+                id("PrepareFilesForSonar")
+                name = "Prepare dot files for sonar scan"
+
+                steps {
+                    script {
+                        name = "Copy files into a temporary location"
+                        scriptContent = """
+                            |#!/bin/bash
+                            | set -x
+                            | cp -R resources resources-sonar-temp
+                            | set +x
+                        """.trimMargin()
+                        workingDir = "."
+                    }
+                    script {
+                        name = "Change dot files into non-dot files"
+                        scriptContent = """
+                            |#!/bin/bash
+                            | set -x
+                            |for filename in $(ls -1a | grep "^\.[a-zA-Z0-9]"); do
+                            |  if [ ! -d "${'$'}filename" ]; then
+                            |      mv ${'$'}filename $(echo ${'$'}filename | sed 's/^.//')
+                            |  fi
+                            |done
+                            | set +x
+                            |ls -lha
+                        """.trimMargin()
+                        workingDir = "resources-sonar-temp"
+                    }
                 }
-                enablePublishMetrics = true
+            }
+
+            makeVerify {
+                buildArtifactRules = listOf(ArtifactRule.include("resources-sonar-temp.zip"))
+
+                sonarScanSettings = {
+                    sonarProjectSources = "resources-sonar-temp/"
+                    sonarProjectTests = "tests/"
+                    additionalParams =
+                        mutableListOf(
+                            "-Dsonar.scm.exclusions.disabled=true",
+                            "-X"
+                        )
+                    outputArtifactRules = listOf(
+                        ArtifactRule(
+                            include = true,
+                            src = "resources-sonar-temp.zip!**",
+                            dst = "resources-sonar-temp"
+                        )
+                    )
+                }
+                enablePublishMetrics = false
                 publishMetricsSettings = {
                     skipCodeCoverage = true
+                }
+                disableLint = true
+            }.apply {
+                this.buildType.dependencies {
+                    artifacts(buildTypeId = prepareFilesForSonarJob.id) {
+                        buildRule = lastFinished("%teamcity.build.branch%")
+                        rules =  listOf(ArtifactRule.include("resources-sonar-temp.zip"))
+                        cleanDestination = false
+                    }
                 }
             }
         }
     }
+
 }
