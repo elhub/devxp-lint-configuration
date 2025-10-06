2d1735ebca2e4358544fdac1e9690dd8e4919e1b
Brian Joseph Donohoe <brian.donohoe@statnett.no>
2025-10-06 12:30:30 +0200

    Refactor custom job to a function

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,45 +2,16 @@ import jetbrains.buildServer.configs.kotlin.ArtifactRule
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import no.elhub.devxp.build.configuration.pipeline.constants.AgentScope
 import no.elhub.devxp.build.configuration.pipeline.constants.Group.DEVXP
+import no.elhub.devxp.build.configuration.pipeline.dsl.PipelineScope
 import no.elhub.devxp.build.configuration.pipeline.dsl.elhubProject
+import no.elhub.devxp.build.configuration.pipeline.jobs.VerifyArtifacts
 import no.elhub.devxp.build.configuration.pipeline.jobs.customJob
 import no.elhub.devxp.build.configuration.pipeline.jobs.makeVerify
 
 elhubProject(DEVXP, "devxp-lint-configuration") {
     pipeline {
         sequential {
-            val prepareFilesForSonarJob = customJob(
-                AgentScope.LinuxAgentContext,
-                buildArtifactRules = listOf(ArtifactRule.include("resources/dotfiles", "dotfiles.zip")),
-                outputArtifactRules = listOf(ArtifactRule.include("dotfiles.zip", "resources/dotfiles"))
-            ) {
-                id("PrepareFilesForSonar")
-                name = "📃 Prepare dotfiles for SonarScan"
-
-                steps {
-                    script {
-                        name = "Copy dotfiles into a temporary location for scanning by sonar"
-                        scriptContent = """
-                            |#!/bin/bash
-                            |mkdir dotfiles
-                            |cp ./.* dotfiles || echo "Ignoring errors for copy"
-                        """.trimMargin()
-                        workingDir = "resources"
-                    }
-                    script {
-                        name = "Change dot files into non-dot files (So sonar will report them)"
-                        scriptContent = """
-                            |#!/bin/bash
-                            |for filename in $(ls -1a | grep "^\.[a-zA-Z0-9]"); do
-                            |  if [ ! -d "${'$'}filename" ]; then
-                            |      mv ${'$'}filename $(echo ${'$'}filename | sed 's/^.//')
-                            |  fi
-                            |done
-                        """.trimMargin()
-                        workingDir = "resources/dotfiles"
-                    }
-                }
-            }
+            val prepareDotfilesForSonarScanJob = prepareDotfilesForSonarScan()
 
             makeVerify {
                 buildArtifactRules = listOf(ArtifactRule.include("dotfiles.zip"))
@@ -69,13 +40,46 @@ elhubProject(DEVXP, "devxp-lint-configuration") {
                 }
             }.apply {
                 this.buildType.dependencies {
-                    artifacts(buildTypeId = prepareFilesForSonarJob.id) {
+                    artifacts(buildTypeId = prepareDotfilesForSonarScanJob.id) {
                         buildRule = lastFinished("%teamcity.build.branch%")
                         rules = listOf(ArtifactRule.include("dotfiles.zip"))
                         cleanDestination = false
                     }
                 }
             }
+        }
+    }
+}
+
+fun PipelineScope.prepareDotfilesForSonarScan(): VerifyArtifacts = customJob(
+    AgentScope.LinuxAgentContext,
+    buildArtifactRules = listOf(ArtifactRule.include("resources/dotfiles", "dotfiles.zip")),
+    outputArtifactRules = listOf(ArtifactRule.include("dotfiles.zip", "resources/dotfiles"))
+) {
+    id("PrepareFilesForSonar")
+    name = "📃 Prepare dotfiles for SonarScan"
+
+    steps {
+        script {
+            name = "Copy dotfiles into a temporary location for scanning by sonar"
+            scriptContent = """
+                            |#!/bin/bash
+                            |mkdir dotfiles
+                            |cp ./.* dotfiles || echo "Ignoring errors for copy"
+            """.trimMargin()
+            workingDir = "resources"
+        }
+        script {
+            name = "Change dot files into non-dot files (So sonar will report them)"
+            scriptContent = """
+                            |#!/bin/bash
+                            |for filename in $(ls -1a | grep "^\.[a-zA-Z0-9]"); do
+                            |  if [ ! -d "${'$'}filename" ]; then
+                            |      mv ${'$'}filename $(echo ${'$'}filename | sed 's/^.//')
+                            |  fi
+                            |done
+            """.trimMargin()
+            workingDir = "resources/dotfiles"
         }
     }
 }
